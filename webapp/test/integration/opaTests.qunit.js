5a63ed263ed21ef81f202bc63fc6f7474ff0442d
Manuel <manuel.bernal@sap.com>
2022-11-11 00:49:52 +0000

    Push project contentto github

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["p2c/modhelloui5/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"p2c/modhelloui5/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
